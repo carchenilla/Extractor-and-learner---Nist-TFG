0103e44785c12ad7aba4f6efdd3c2a8c296489ce
carchenilla <sergiomichiels@gmail.com>
2016-10-06 20:42:23 +0200

    Commit 6/10/2016
    - svm
        - A module which uses sklearn.svm to perform a multi-class classification using a support vector machine
        - Supports different kernels and parameters
    
    - checkLocalData
        - Changed tests to include one of svm

--- checkLocalData.py
+++ checkLocalData.py
@@ -1,9 +1,11 @@
-from random import sample
 import pickle
 from Knn import kNN
 from pca import pca
+from svm import svm
 import matplotlib.pyplot as plt
 from k_means import elbowCheck, kmeans
+from svm import svm
+
 from data_types.VulnDictionary import VulnDictionary
 
 
@@ -11,6 +13,7 @@ if __name__ == "__main__":
     count = 0
     dictionary_list = []
     vulnerability_list = []
+    test_list = []
     for i in range(2002, 2017):
         try:
             with open("dictionaries/VulnDictionary_"+str(i)+".p", 'rb') as f:
@@ -21,12 +24,18 @@ if __name__ == "__main__":
             dictionary_list.append(VulnDictionary(i))
     print()
     for d in dictionary_list:
-        d.update()
+        #d.update()
         count = count + len(d.dict.keys())
-        vulnerability_list.extend(list(d.dict.values()))
+        if d.year < 2016:
+            vulnerability_list.extend(list(d.dict.values()))
+        else:
+            test_list.extend(list(d.dict.values()))
     print("Total: "+str(count))
 
-    (cost, assig) = kmeans.best_cost_kmeans(vulnerability_list, times=7, k=4)
+
+    svm.run_svm(vulnerability_list, test_list, kernel="rbf", gamma=0.125)
+
+    '''(cost, assig) = kmeans.best_cost_kmeans(vulnerability_list, times=7, k=4)
 
     print("Finished K-means. Saving data to dictionaries and disk")
     for x in assig:
@@ -42,34 +51,5 @@ if __name__ == "__main__":
         except IOError as err:
             print("Error with dictionary "+str(d.year)+" - "+str(err))
 
-    '''print("Done. Now validating results with Knn for 2016")
 
-    train_data = []
-    test_data = []
-    results_list = []
-
-    for d in dictionary_list:
-        if d.year < 2016:
-            train_data.extend(list(d.dict.values()))
-        else:
-            data_2016 = list(d.dict.values())
-
-    samples = sample(range(4000),500)
-
-    for i in range(len(data_2016)):
-        if i in samples:
-            test_data.append(data_2016[i])
-        else:
-            train_data.append(data_2016[i])
-
-    print(len(train_data))
-    print(len(test_data))
-
-
-    for i in range(1,11):
-        print("Check knn for k="+str(i))
-        results_list.append(kNN.knn(train_data, test_data, i))
-
-
-    plt.plot(range(1,11), results_list)
-    plt.show()'''
+    print("Done. Now validating results with Knn for 2016")'''
